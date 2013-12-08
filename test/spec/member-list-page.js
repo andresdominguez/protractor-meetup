50ce0ca1db4ccfa27a39e56eea7ba6e8096243b4
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-08 01:17:16 -0500

    new page object

---
+++ test/spec/member-list-page.js
@@ -0,0 +1,18 @@
+/**
+ * Page object for member list.
+ * @constructor
+ */
+MemberListPage = function() {
+  this.createButton = $('.btn-primary');
+
+  this.navigate = function() {
+    browser.get('#/member-list');
+  };
+
+  this.clickOnRow = function(index) {
+    var list = element.all(by.repeater('item in list'));
+    list.get(index).findElement(by.css('a')).click();
+  }
+};
+
+module.exports =  MemberListPage;

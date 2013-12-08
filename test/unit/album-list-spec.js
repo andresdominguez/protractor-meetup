950e4faf48c7897f4241bc298c5ff7bea9d1a581
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-08 14:12:08 -0500

    new

---
+++ test/unit/album-list-spec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('Controller: AlbumListCtrl', function() {
+
+  beforeEach(module('ProtractorMeetupApp', 'ControllerTestHelper'));
+
+  beforeEach(inject(function(jasmineMatchers) {
+    this.addMatchers(jasmineMatchers);
+  }));
+
+  var AlbumListCtrl, scope, createController, fake;
+
+  beforeEach(inject(function($controller, $rootScope, fakeResource) {
+    fake = fakeResource;
+
+    createController = function() {
+      scope = $rootScope.$new();
+      AlbumListCtrl = $controller('AlbumListCtrl', {
+        $scope: scope
+      });
+    };
+  }));
+
+  it('should request a list of albums', function() {
+    fake.album.whenGetList().returnsDefault();
+
+    // When you create the controller.
+    createController();
+    expect(scope.list).toEqualData([]);
+    fake.flush();
+
+    // Then ensure the list of albums was populated.
+    expect(scope.list.length).toBe(2);
+    expect(fake.album).toHaveBeenRequested();
+  });
+});

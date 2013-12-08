64fe9564fe7ee212be83b6fe09ed45319a32dfdf
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-07 19:36:46 -0500

    new

---
+++ app/services/apiService.js
@@ -0,0 +1,25 @@
+angular.module('angularMeetupApp').
+    factory('apiService', function($resource) {
+      var band = $resource('/bands/:id', {}, {
+        update: {
+          method: 'PUT'
+        }
+      });
+
+      var member = $resource('/members/:id', {}, {
+        update: {
+          method: 'PUT'
+        }
+      });
+      var album = $resource('/albums/:id', {}, {
+        update: {
+          method: 'PUT'
+        }
+      });
+
+      return {
+        album: album,
+        band: band,
+        member: member
+      };
+    });

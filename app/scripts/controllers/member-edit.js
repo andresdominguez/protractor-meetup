0e918742aa99ec2a9278a7cec434a43fad3a92b4
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-08 00:27:27 -0500

    fix formatting

--- app/scripts/controllers/member-edit.js
+++ app/scripts/controllers/member-edit.js
@@ -1,43 +1,40 @@
 'use strict';
 
-angular.module('ProtractorMeetupApp')
-    .controller('MemberEditCtrl', function($scope, $routeParams, apiService, $location) {
+angular.module('ProtractorMeetupApp').controller('MemberEditCtrl',
+    function($scope, $routeParams, apiService, $location) {
+  var memberId = $routeParams.memberId;
 
-      var memberId = $routeParams.memberId;
-
-      if (memberId !== 'new') {
-        apiService.member.get({id: memberId}, function(data) {
-          $scope.item = data;
-        });
-      }
-
-      var handleError = function(response) {
-        $scope.error = response.data;
-      };
-
-      // Delete the member.
-      $scope.remove = function() {
-        apiService.member.remove({id: memberId}, function() {
-          $location.path('/member-list');
-        }, handleError);
-      };
-
-      $scope.save = function() {
-        $scope.message = '';
-
-        var isNew = memberId === 'new',
-            item = angular.copy($scope.item);
-
-        if (isNew) {
-          apiService.member.save($scope.item,
-              function(newItem) {
-                $location.path('/member-edit/' + newItem._id);
-              }, handleError);
-        } else {
-          apiService.member.update({id: memberId}, _.omit(item, '_id'),
-              function() {
-                $scope.message = 'Member updated';
-              }, handleError);
-        }
-      };
+  if (memberId !== 'new') {
+    apiService.member.get({id: memberId}, function(data) {
+      $scope.item = data;
     });
+  }
+
+  var handleError = function(response) {
+    $scope.error = response.data;
+  };
+
+  // Delete the member.
+  $scope.remove = function() {
+    apiService.member.remove({id: memberId}, function() {
+      $location.path('/member-list');
+    }, handleError);
+  };
+
+  $scope.save = function() {
+    $scope.message = '';
+
+    var isNew = memberId === 'new',
+        item = angular.copy($scope.item);
+
+    if (isNew) {
+      apiService.member.save($scope.item, function(newItem) {
+        $location.path('/member-edit/' + newItem._id);
+      }, handleError);
+    } else {
+      apiService.member.update({id: memberId}, _.omit(item, '_id'), function() {
+        $scope.message = 'Member updated';
+      }, handleError);
+    }
+  };
+});

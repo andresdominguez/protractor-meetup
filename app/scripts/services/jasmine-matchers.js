6d527e930c389cfafcc3ebec42115e4ac47e7e2c
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-08 14:03:59 -0500

    new

---
+++ app/scripts/services/jasmine-matchers.js
@@ -0,0 +1,86 @@
+angular.module('ProtractorMeetupApp').factory('jasmineMatchers', function() {
+
+  /**
+   * Ensure the spy for the resource method ws created. It will add an error
+   * message when you don't have a spy.
+   *
+   * @param {!Object} scope A reference to the jasmine's 'this'.
+   * @param {Object} spy The spy on the resource method.
+   * @returns {boolean} False when the spy is not defined.
+   */
+  var verifySpy = function(scope, spy) {
+    if (!spy) {
+      scope.message = function() {
+        return 'You did not set a when or expect for the resource';
+      };
+      return false;
+    }
+    return true;
+  };
+
+  return {
+    toEqualData: function(expected) {
+      return angular.equals(this.actual, expected);
+    },
+    toHaveBeenRequested: function() {
+      var spy = this.actual.getSpy();
+      if (!verifySpy(this, spy)) {
+        return false;
+      }
+
+      return spy.callCount > 0;
+    },
+    toHaveBeenCreated: function() {
+      var spy = this.actual.getCreateSpy();
+      if (!verifySpy(this, spy)) {
+        return false;
+      }
+
+      var wasCalled = spy.callCount > 0;
+
+      if (!wasCalled) {
+        this.message = function() {
+          return 'The item was not created';
+        };
+      }
+
+      return  wasCalled;
+    },
+    toHaveBeenUpdated: function() {
+      var spy = this.actual.getUpdateSpy();
+      if (!verifySpy(this, spy)) {
+        return false;
+      }
+
+      var wasCalled = spy.callCount > 0;
+
+      if (!wasCalled) {
+        this.message = function() {
+          return 'The resource was not updated';
+        };
+      }
+
+      return  wasCalled;
+    },
+    toHaveBeenUpdatedWith: function(expected) {
+      var spy = this.actual.getUpdateSpy();
+      if (!verifySpy(this, spy)) {
+        return false;
+      }
+
+      var item = spy.mostRecentCall.args[1];
+
+      var allPropertiesMatch = _.all(expected, function(value, key) {
+        return angular.equals(value, item[key]);
+      });
+
+      if (!allPropertiesMatch) {
+        this.message = function() {
+          return 'Expected ' + JSON.stringify(item) + ' to match ' + JSON.stringify(expected);
+        };
+      }
+
+      return allPropertiesMatch;
+    }
+  };
+});
---
+++ app/scripts/services/mocks.js
@@ -0,0 +1,45 @@
+angular.module('ProtractorMeetupApp').factory('mocks', function() {
+  return {
+    album: {
+      getById: function() {
+        return {
+          name: 'Licensed to Ill',
+          year: 1986
+        };
+      },
+      getList: function() {
+        return [
+          {name: 'Check your head'},
+          {name: 'Pauls boutique'}
+        ];
+      }
+    },
+    band: {
+      getById: function() {
+        return {
+          name: 'Beastie boys',
+          albums: [],
+          members: []
+        };
+      },
+      getList: function() {
+        return [
+          {name: 'Wu-Tang clan'},
+          {name: 'Beastie boys'}
+        ];
+      }
+    },
+    member: {
+      getById: function() {
+        return {name: 'Method Man'};
+      },
+      getList: function() {
+        return [
+          {name: 'Michael "Mike D" Diamond'},
+          {name: 'Adam "Ad-Rock" Horovitz'},
+          {name: 'Adam "MCA" Yauch'}
+        ];
+      }
+    }
+  };
+});

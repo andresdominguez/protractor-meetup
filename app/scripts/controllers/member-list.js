63b055ecc91e8618a93de827d69b564865bd10bd
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-12-15 13:48:31 -0500

    Add loading flag

--- app/scripts/controllers/member-list.js
+++ app/scripts/controllers/member-list.js
@@ -2,7 +2,10 @@
 
 angular.module('ProtractorMeetupApp').
     controller('MemberListCtrl', function($scope, apiService) {
+      $scope.loading = true;
+
       apiService.member.query({}, function(data) {
+        $scope.loading = false;
         $scope.list = data;
       });
     });
